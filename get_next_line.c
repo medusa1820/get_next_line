85a2a13b0a55c51a010b0dafb1b85dc0aef12650
Alvin J. Alexander <alvin@alexander.jp>
2023-02-18 00:52:36 +0100

    all work, but veeery slow, dont know if intra's testing system has time limitations

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: musenov <musenov@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/11 14:21:17 by musenov           #+#    #+#             */
-/*   Updated: 2023/02/17 23:22:23 by musenov          ###   ########.fr       */
+/*   Updated: 2023/02/18 00:48:48 by musenov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,48 +51,88 @@ char	*check_remainder(char **remainder, char **line)
 	return (p_n);
 }
 
-char	*bwr_greater_zero(char **line, char **p_buf, char **p_n, char **remainder)
+// char	*bwr_greater_zero(char **line, char **p_buf, char **p_n, char **remainder)
+// {
+// 	*p_n = ft_strchr(*p_buf, '\n');
+// 	if (*p_n)
+// 	{
+// 		*remainder = ft_strdup(++*p_n);
+// 		if (!*remainder)
+// 			return (free(*line), free(*remainder), *remainder = NULL, NULL);
+// 		**p_n = '\0';
+// 	}
+// 	*line = ft_strjoin(*line, *p_buf);
+// 	if (!*line)
+// 		return (free(*remainder), *remainder = NULL, NULL);
+// 	return (*line);
+// }
+
+char	*read_to_buf(char **remainder, char **line, char **p_n, int *fd)
 {
-	*p_n = ft_strchr(*p_buf, '\n');
-	if (*p_n)
+	char	buf[BUFFER_SIZE + 1];
+	int		bwr;
+
+	bwr = read(*fd, buf, BUFFER_SIZE);
+	if ((bwr < 0) || (bwr == 0 && **line == '\0'))
+		return (free(*line), NULL);
+	if (bwr == 0 && **line != '\0')
+		return (*line);
+	if (bwr > 0)
 	{
-		*remainder = ft_strdup(++*p_n);
-		if (!*remainder)
-			return (free(*line), free(*remainder), *remainder = NULL, NULL);
-		**p_n = '\0';
+		// buf[bwr] = '\0';
+		// p_buf = buf;
+		// if (!(bwr_greater_zero(&line, &p_buf, &p_n, &remainder)))
+		// 	return (NULL);
+		buf[bwr] = '\0';
+		*p_n = ft_strchr(buf, '\n');
+		if (*p_n)
+		{
+			*remainder = ft_strdup(++*p_n);
+			if (!*remainder)
+				return (free(*line), free(*remainder), *remainder = NULL, NULL);
+			**p_n = '\0';
+		}
+		*line = ft_strjoin(*line, buf);
+		if (!*line)
+			return (free(*remainder), *remainder = NULL, NULL);
 	}
-	*line = ft_strjoin(*line, *p_buf);
-	if (!*line)
-		return (free(*remainder), *remainder = NULL, NULL);
 	return (*line);
 }
 
 char	*get_next_line(int fd)
 {
 	static char		*remainder;
-	char			buf[BUFFER_SIZE + 1];
-	int				bwr;
+	// char			buf[BUFFER_SIZE + 1];
+	// int				bwr;
 	char			*p_n;
 	char			*line;
-	char			*p_buf;
+	// char			*p_buf;
 
 	p_n = check_remainder(&remainder, &line);
 	if (!line)
 		return (NULL);
 	while (!p_n)
 	{
-		bwr = read(fd, buf, BUFFER_SIZE);
-		if ((bwr < 0) || (bwr == 0 && *line == '\0'))
-			return (free(line), NULL);
-		if (bwr == 0 && *line != '\0')
-			return (line);
-		if (bwr > 0)
-		{
-			buf[bwr] = '\0';
-			p_buf = buf;
-			if (!(bwr_greater_zero(&line, &p_buf, &p_n, &remainder)))
-				return (NULL);
-		}
+		// bwr = read(fd, buf, BUFFER_SIZE);
+		// if ((bwr < 0) || (bwr == 0 && *line == '\0'))
+		// 	return (free(line), NULL);
+		// if (bwr == 0 && *line != '\0')
+		// 	return (line);
+		// if (bwr > 0)
+		// {
+		// 	buf[bwr] = '\0';
+		// 	p_buf = buf;
+		// 	if (!(bwr_greater_zero(&line, &p_buf, &p_n, &remainder)))
+		// 		return (NULL);
+		// }
+		
+		
+		// if (!(read_to_buf(&remainder, &line, &p_n, fd)))
+		// 	return (NULL);
+
+		line = read_to_buf(&remainder, &line, &p_n, &fd);
+		if (!line)
+			return (NULL);
 	}
 	return (line);
 }
