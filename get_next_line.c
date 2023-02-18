65486477ba167ae0b169b6d14a69c31400ea9c04
Alvin J. Alexander <alvin@alexander.jp>
2023-02-18 17:21:57 +0100

    mandatory ready

--- get_next_line.c
+++ get_next_line.c
@@ -1,93 +1,85 @@
 #include "get_next_line.h"
 
-char	*check_remainder(char **remainder, char **line, char **p_n)
-{
-	char	*tmp;
-
-	*p_n = NULL;
-	if (*remainder)
-	{
-		*p_n = ft_strchr(*remainder, '\n');
-		if (*p_n)
-		{
-			tmp = ft_strdup(++*p_n);
-			if (!tmp)
-				return (free(*remainder), *remainder = NULL, *line = NULL, NULL);
-			**p_n = '\0';
-			*line = ft_strdup(*remainder);
-			if (!*line)
-				return (free(tmp), tmp = NULL, free(*remainder), *remainder = NULL, NULL);
-			free(*remainder);
-			*remainder = tmp;
-		}
-		else
-		{
-			*line = ft_strdup(*remainder);
-			free(*remainder);
-			*remainder = NULL;
-			if (!*line)
-				return (NULL);
-		}
-	}
-	else
-	{
-		*line = ft_strnew(1);
-		if (!*line)
-			return (NULL);
-	}
-	return (*p_n);
-}
-
-int	read_to_buf(char **remainder, char **line, char **p_n, int *fd)
+int	read_to_buf(char **left, char **line, char **p_n, int *fd)
 {
 	char	buf[BUFFER_SIZE + 1];
-	int		bwr;
+	int		read_len;
 
-	bwr = read(*fd, buf, BUFFER_SIZE);
-	if ((bwr < 0) || (bwr == 0 && **line == '\0'))
-		return (bwr);
-	if (bwr == 0 && **line != '\0')
-		return (bwr);
-	if (bwr > 0)
+	read_len = read(*fd, buf, BUFFER_SIZE);
+	if ((read_len < 0) || (read_len == 0 && **line == '\0'))
+		return (read_len);
+	if (read_len == 0 && **line != '\0')
+		return (read_len);
+	if (read_len > 0)
 	{
-		buf[bwr] = '\0';
+		buf[read_len] = '\0';
 		*p_n = ft_strchr(buf, '\n');
 		if (*p_n)
 		{
-			*remainder = ft_strdup(++*p_n);
-			if (!*remainder)
-				return (bwr);
+			*left = ft_strdup(++*p_n);
+			if (!*left)
+				return (read_len);
 			**p_n = '\0';
 		}
 		*line = ft_strjoin(*line, buf);
 		if (!*line)
-			return (bwr);
+			return (read_len);
 	}
-	return (bwr);
+	return (read_len);
+}
+
+char	*check_left(char **left, char **line, char **p_n)
+{
+	char	*tmp;
+
+	if (*left)
+		*p_n = ft_strchr(*left, '\n');
+	if (*left && *p_n)
+	{
+		tmp = ft_strdup(++*p_n);
+		if (!tmp)
+			return (free(*left), *left = NULL, *line = NULL, NULL);
+		**p_n = '\0';
+		*line = ft_strdup(*left);
+		if (!*line)
+			return (free(tmp), tmp = NULL, free(*left), *left = NULL, NULL);
+		free(*left);
+		*left = tmp;
+	}
+	else if (*left && !*p_n)
+	{
+		*line = ft_strdup(*left);
+		free(*left);
+		*left = NULL;
+	}
+	else
+		*line = ft_strnew(1);
+	return (*p_n);
 }
 
 char	*get_next_line(int fd)
 {
-	static char		*remainder;
+	static char		*left;
 	char			*p_n;
 	char			*line;
-	int				bwr;
+	int				read_len;
 
-	if (!(check_remainder(&remainder, &line, &p_n)) && !line)
+	p_n = NULL;
+	if (!(check_left(&left, &line, &p_n)) && !line)
 		return (NULL);
 	while (!p_n)
 	{
-		bwr = read_to_buf(&remainder, &line, &p_n, &fd);
-		if ((bwr < 0) || (bwr == 0 && *line == '\0'))
+		read_len = read_to_buf(&left, &line, &p_n, &fd);
+		if ((read_len < 0) || (read_len == 0 && *line == '\0'))
 			return (free(line), NULL);
-		if (bwr == 0 && *line != '\0')
+		if (read_len == 0 && *line != '\0')
 			return (line);
-		if (bwr > 0)
+		if (read_len > 0)
 		{
-			if (p_n && !remainder)
-				return (free(line), free(remainder), remainder = NULL, NULL);
+			if (p_n && !left)
+				return (free(line), free(left), left = NULL, NULL);
 			if (!line)
-				return (free(remainder), remainder = NULL, NULL);
+				return (free(left), left = NULL, NULL);
 		}
 	}
 	return (line);
